470ea2f12ec63da02da8569c5c3c3bd6dc10a1b4
thibhero <thibaut.herault@gmail.com>
2025-03-17 17:17:49 -0400

    ARINC429 class

--- arinc429.py
+++ arinc429.py
@@ -5,152 +5,177 @@ ALTITUDE_MAX = 40000
 SigBits = 15
 
 
-def get_parity(x: int) -> int:
-    result = 0
-    while x:
-        result ^= x & 1
-        x >>= 1
-    return result ^ 1
-    # or return bin(x).count("1") % 2
+class ARINC429:
+    ON_GROUND = 0
+    ALTITUDE_CHANGE = 1
+    CRUISE = 2
+    ALTITUDE_MAX = 40000
+    SigBits = 15
+
+    @staticmethod
+    def __get_parity(x: int) -> int:
+        result = 0
+        while x:
+            result ^= x & 1
+            x >>= 1
+        return result ^ 1
+        # or return bin(x).count("1") % 2
+
+    @staticmethod
+    def check_parity(x: int) -> str:
+        return "Even" if ARINC429.__get_parity(x) ^ 1 == 0 else "Odd"
+
+    @staticmethod
+    def is_valid(x: int) -> bool:
+        return ARINC429.__get_parity(x >> 1) == x & 1
 
 
-def check_parity(x: int) -> str:
-    return "Even" if get_parity(x) ^ 1 == 0 else "Odd"
+    @staticmethod
+    def __reverse_bits(x: int, num_bits: int) -> int:
+        return sum(((x >> i) & 1) << (num_bits - 1 - i) for i in range(num_bits))
+
+    @staticmethod
+    def __encode_001(altitude: int, state: int) -> (int, int):
+        if altitude is None:
+            return (1, state) if state == ON_GROUND else (0, 0)
+        if abs(altitude) > ALTITUDE_MAX:
+            return 0, 0
+        sign = 1 if altitude < 0 else 0
+        altitude = abs(altitude)
+
+        out = sign << (SigBits + 1)
+        k = SigBits
+        while altitude > 1 and k >= 0:
+            step = ALTITUDE_MAX / (2 ** (SigBits - k))
+            if altitude >= step:
+                out |= 1 << k
+                altitude -= step
+            k -= 1
+
+        out <<= 2
+
+        if state in (ON_GROUND, ALTITUDE_CHANGE, CRUISE):
+            out |= state
+            return 3, out
+
+        return 0, out
 
 
-def is_valid(x: int) -> bool:
-    return get_parity(x >> 1) == x & 1
+    @staticmethod
+    def __decode_001(ssm: int, data: int) -> (int, int):
+        if ssm == 0:
+            return None, None
+        if ssm == 1:
+            return None, data  # data is only state in that case
+
+        state = data & 3
+        data >>= 2
+
+        altitude = 0
+        for k in range(SigBits + 1):
+            altitude += (data & 1) * (ALTITUDE_MAX / (2 ** (SigBits - k)))
+            data >>= 1
+
+        if data:
+            altitude = -altitude
+
+        return altitude, state
 
 
-def reverse_bits(x: int, num_bits: int) -> int:
-    return sum(((x >> i) & 1) << (num_bits - 1 - i) for i in range(num_bits))
+    @staticmethod
+    def __encode_002(rise_rate: float) -> (int, int):
+        if rise_rate is None:
+            return 1, 0
+        ssm = 0
+        if rise_rate < 0:
+            ssm = 3
+        rise_rate = abs(rise_rate)
+        rise_rate_bits = (
+                (int(rise_rate // 100) << 12)
+                | ((int(rise_rate // 10) % 10) << 8)
+                | (int(rise_rate % 10) << 4)
+                | (int(rise_rate * 10) % 10)
+        )
+        return ssm, rise_rate_bits
 
+    @staticmethod
+    def __decode_002(ssm: int, data: int) -> float:
+        if ssm == 1:
+            return 0.0
 
-def encode(label: int, sdi: int, data: int, ssm: int) -> int:
-    label_bits = ((label // 100) << 6) | (((label // 10) % 10) << 3) | (label % 10)
-    label_reversed = reverse_bits(label_bits, 8)
+        rise_rate = (
+                            (data & 0x0F)
+                            + ((data >> 4) & 0x0F) * 10
+                            + ((data >> 8) & 0x0F) * 100
+                            + ((data >> 12) & 0x0F) * 1000
+                    ) / 10
+        if ssm == 3:
+            rise_rate = -rise_rate
+        return rise_rate
 
-    result = label_reversed << 24
-    result |= reverse_bits(sdi, 2) << 22
-    result |= reverse_bits(data, 19) << 3
-    result |= reverse_bits(ssm, 2) << 1
+    @staticmethod
+    def __encode_003(angle: float) -> (int, int):
+        if angle is None:
+            return 1, 0
+        ssm = 0
+        if angle < 0:
+            ssm = 3
+        angle = abs(angle)
+        angle_bits = (
+                (int(angle // 10) << 8) | (int(angle % 10) << 4) | (int(angle * 10) % 10)
+        )
+        print(bin(angle_bits))
+        return ssm, angle_bits
 
-    result |= get_parity(result)
+    @staticmethod
+    def __decode_003(ssm: int, data: int) -> float:
+        if ssm == 1:
+            return 0.0
+        angle = ((data & 0x0F) + ((data >> 4) & 0x0F) * 10 + ((data >> 8) & 1) * 100) / 10
+        if ssm == 3:
+            angle = -angle
+        return angle
 
-    return result
+    __encodes = [__encode_001, __encode_002, __encode_003]
+    __decodes = [__decode_001, __decode_002, __decode_003]
 
+    @staticmethod
+    def encode(label: int, sdi: int, *args) -> int:
+        if label - 1 not in range(len(ARINC429.__encodes)):
+            return 0
 
-def decode(data: int):
-    if not is_valid(data):
-        return "Data is not valid"
-    ssm = data >> 1 & 0xF
-    data_out = data >> 3 & 0x7FFFF
-    sdi = data >> 22 & 0xF
-    label = data >> 24 & 0xFF
-    ssm = reverse_bits(ssm, 2)
-    data_out = reverse_bits(data_out, 19)
-    sdi = reverse_bits(sdi, 2)
-    label = reverse_bits(label, 8)
-    label_out = (
-        (label & 0x07) + ((label >> 3) & 0x07) * 10 + ((label >> 6) & 0x03) * 100
-    )
-    return label_out, sdi, data_out, ssm
+        ssm, data = ARINC429.__encodes[label - 1](*args)
 
+        label_bits = ((label // 100) << 6) | (((label // 10) % 10) << 3) | (label % 10)
+        label_reversed = ARINC429.__reverse_bits(label_bits, 8)
 
-def encode_001(altitude: int, state: int) -> (int, int):
-    if altitude is None:
-        return (1, state) if state == ON_GROUND else (0, 0)
+        result = label_reversed << 24
+        result |= ARINC429.__reverse_bits(sdi, 2) << 22
+        result |= ARINC429.__reverse_bits(data, 19) << 3
+        result |= ARINC429.__reverse_bits(ssm, 2) << 1
 
-    sign = 1 if altitude < 0 else 0
-    altitude = abs(altitude)
+        result |= ARINC429.__get_parity(result)
 
-    out = sign << (SigBits + 1)
-    k = SigBits
-    while altitude > 1 and k >= 0:
-        step = ALTITUDE_MAX / (2 ** (SigBits - k))
-        if altitude >= step:
-            out |= 1 << k
-            altitude -= step
-        k -= 1
+        return result
 
-    out <<= 2
+    @staticmethod
+    def decode(data: int):
+        if not ARINC429.is_valid(data):
+            return "Data is not valid"
+        ssm = data >> 1 & 0xF
+        data_out = data >> 3 & 0x7FFFF
+        sdi = data >> 22 & 0xF
+        label = data >> 24 & 0xFF
+        ssm = ARINC429.__reverse_bits(ssm, 2)
+        data_out = ARINC429.__reverse_bits(data_out, 19)
+        sdi = ARINC429.__reverse_bits(sdi, 2)
+        label = ARINC429.__reverse_bits(label, 8)
+        label_out = (
+                (label & 0x07) + ((label >> 3) & 0x07) * 10 + ((label >> 6) & 0x03) * 100
+        )
+        if label_out - 1 not in range(len(ARINC429.__decodes)):
+            return "Data is not valid"
 
-    if state in (ON_GROUND, ALTITUDE_CHANGE, CRUISE):
-        out |= state
-        return 3, out
+        out = ARINC429.__decodes[label_out - 1](ssm, data_out)
 
-    return 0, out
-
-
-def decode_001(ssm: int, data: int) -> (int, int):
-    if ssm == 0:
-        return None, None
-    if ssm == 1:
-        return None, data  # data is only state in that case
-
-    state = data & 3
-    data >>= 2
-
-    altitude = 0
-    for k in range(SigBits + 1):
-        altitude += (data & 1) * (ALTITUDE_MAX / (2 ** (SigBits - k)))
-        data >>= 1
-
-    if data:
-        altitude = -altitude
-
-    return altitude, state
-
-
-def encode_002(rise_rate: float) -> (int, int):
-    if rise_rate is None:
-        return 1, 0
-    ssm = 0
-    if rise_rate < 0:
-        ssm = 3
-    rise_rate = abs(rise_rate)
-    rise_rate_bits = (
-        (int(rise_rate // 100) << 12)
-        | ((int(rise_rate // 10) % 10) << 8)
-        | (int(rise_rate % 10) << 4)
-        | (int(rise_rate * 10) % 10)
-    )
-    return ssm, rise_rate_bits
-
-
-def decode_002(ssm: int, data: int) -> float:
-    if ssm == 1:
-        return 0.0
-
-    rise_rate = (
-        (data & 0x0F)
-        + ((data >> 4) & 0x0F) * 10
-        + ((data >> 8) & 0x0F) * 100
-        + ((data >> 12) & 0x0F) * 1000
-    ) / 10
-    if ssm == 3:
-        rise_rate = -rise_rate
-    return rise_rate
-
-
-def encode_003(angle: float) -> (int, int):
-    if angle is None:
-        return 1, 0
-    ssm = 0
-    if angle < 0:
-        ssm = 3
-    angle = abs(angle)
-    angle_bits = (
-        (int(angle // 10) << 8) | (int(angle % 10) << 4) | (int(angle * 10) % 10)
-    )
-    print(bin(angle_bits))
-    return ssm, angle_bits
-
-
-def decode_003(ssm: int, data: int) -> float:
-    if ssm == 1:
-        return 0.0
-    angle = ((data & 0x0F) + ((data >> 4) & 0x0F) * 10 + ((data >> 8) & 1) * 100) / 10
-    if ssm == 3:
-        angle = -angle
-    return angle
+        return label_out, sdi, ssm, out
--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-import arinc429
+from arinc429 import ARINC429
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -12,31 +12,32 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print(bin(arinc429.encode(325, 2, 1902, 3)))
-    print(arinc429.check_parity(arinc429.encode(325, 1, 2, 3)))
+
+    print(bin(ARINC429.encode(325, 2, 1902, 3)))
+    print(ARINC429.check_parity(ARINC429.encode(325, 1, 2, 3)))
     print_hi('Thibaut')
     word = 0b01100000000000000000000000000110  # Example ARINC 429 word
-    print(arinc429.check_parity(word))
-    print(arinc429.is_valid(word))
-    print(arinc429.decode(arinc429.encode(325, 2, 1902, 3)))
+    print(ARINC429.check_parity(word))
+    print(ARINC429.is_valid(word))
+    print(ARINC429.decode(ARINC429.encode(1, 2, 300000, ARINC429.ON_GROUND)))
 
-    ssm, data = arinc429.encode_001(-30000, arinc429.CRUISE)
-    print(bin(ssm), bin(data))
-    altitude, state = arinc429.decode_001(ssm, data)
-    print(altitude, state)
-
-    ssm, data = arinc429.encode_002(-391.2)
-    print(bin(ssm), bin(data))
-    rate = arinc429.decode_002(ssm, data)
-    print(rate)
-
-    ssm, data = arinc429.encode_003(-2.1)
-    print(bin(ssm), bin(data))
-    word = arinc429.encode(3, 1, data, ssm)
-    print(bin(word))
-    print(arinc429.is_valid(word))
-    label, sdi, data, ssm = arinc429.decode(word)
-    print(label, sdi)
-    print(arinc429.decode_003(ssm, data))
+    # ssm, data = ARINC429.encode_001(-30000, ARINC429.CRUISE)
+    # print(bin(ssm), bin(data))
+    # altitude, state = ARINC429.decode_001(ssm, data)
+    # print(altitude, state)
+    #
+    # ssm, data = ARINC429.encode_002(-391.2)
+    # print(bin(ssm), bin(data))
+    # rate = ARINC429.decode_002(ssm, data)
+    # print(rate)
+    #
+    # ssm, data = ARINC429.encode_003(-2.1)
+    # print(bin(ssm), bin(data))
+    # word = ARINC429.encode(3, 1, data, ssm)
+    # print(bin(word))
+    # print(ARINC429.is_valid(word))
+    # label, sdi, data, ssm = ARINC429.decode(word)
+    # print(label, sdi)
+    # print(ARINC429.decode_003(ssm, data))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
